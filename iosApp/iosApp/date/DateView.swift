9f2180016ebc3ad8d1b9282d703e3f1807ca9dc9
Artem Zasypalov <a.zasypalov@kingbird.ru>
2020-06-27 14:13:06 +0400

    SwiftUI transition

---
+++ iosApp/iosApp/date/DateView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  MultiplatformKtor
+//
+//  Created by Артем on 27.06.2020.
+//  Copyright © 2020 Gaoyun. All rights reserved.
+//
+
+import SwiftUI
+import app
+
+struct DateView: View {
+    
+    //let iosDateUtilities = IosDateUtilities()
+    //iosDateUtilities.initPlatformActual()
+    
+    //let presenter = DatePresenter(view: self)
+    //presenter.doInit()
+    
+    //func showDate(date: String) {
+    //    label.text = date
+    //}
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+struct DateView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateView()
+    }
+}
